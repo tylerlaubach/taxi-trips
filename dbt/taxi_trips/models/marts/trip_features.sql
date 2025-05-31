518b37086244d5bf1504c895ccfe0172ce2537b7
Tyler Laubach <tyler.laubach@gmail.com>
2025-05-31 15:44:06 -0400

    add trip_features mart

---
+++ dbt/taxi_trips/models/marts/trip_features.sql
@@ -0,0 +1,26 @@
+{{ 
+    config(
+      materialized = 'view'
+    ) 
+}}
+
+WITH base AS (
+    SELECT
+         *
+    FROM {{ ref('stg_yellow_taxi') }}
+)
+
+SELECT
+    *
+    , (EXTRACT(epoch FROM dropoff_ts) 
+        - EXTRACT(epoch FROM pickup_ts)
+        ) / 60.0 AS trip_duration_min
+    , EXTRACT(hour FROM pickup_ts) AS pickup_hour
+    , EXTRACT(dow  FROM pickup_ts) AS pickup_weekday
+    , CASE
+        WHEN trip_distance_miles < 2 THEN 'short'
+        WHEN trip_distance_miles < 5 THEN 'medium'
+        ELSE 'long'
+        END AS trip_distance_category
+    , fare_amount / NULLIF(trip_distance_miles, 0) AS fare_per_mile
+FROM base
